d61ee5f8af868b035d03ecdad383ca4800228e2d
Tateishi Tadatoshi <ishio39@gmail.com>
2024-08-03 22:51:08 +0900

    add test. tutorial 5

--- hello/polls/views.py
+++ hello/polls/views.py
@@ -6,13 +6,17 @@ from django.views import generic
 
 from .models import Choice, Question
 
+from django.utils import timezone
+
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
 class DetailView(generic.DetailView):
